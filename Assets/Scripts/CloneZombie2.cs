83b1f343a66d6a8d89322c5c17dfd946bc9fba30
WebFan37 <118756551+WebFan37@users.noreply.github.com>
2024-12-03 10:03:18 -0500

    Add script zombie spawn again

--- Assets/Scripts/CloneZombie2.cs
+++ Assets/Scripts/CloneZombie2.cs
@@ -9,23 +9,34 @@ public class CloneZombie2 : MonoBehaviour
     public GameObject zombieClone;
     public float delayTime = 5f;
 
+    private int maxZombies = 10;
+    private int currentZombies = 0;
 
+    private bool peutSpawn = true;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 endroit1 = new Vector3(Random.Range(321, 330), 0.12f, Random.Range(605, 614));
+       
 
     }
     void Update()
     {
+        if (!peutSpawn) return;
+
         delayTime -= Time.deltaTime;
-        if (delayTime <= 0)
+        if (delayTime <= 0 && currentZombies < maxZombies)
         {
             Vector3 endroit1 = new Vector3(Random.Range(321, 330), 0.12f, Random.Range(605, 614));
             Instantiate(zombieClone, endroit1, Quaternion.identity);
+
+            currentZombies++;
             delayTime = 5f;
+        }
+        if (currentZombies == maxZombies)
+        {
+            peutSpawn = false;
         }
     }
 
